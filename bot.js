e39a3bafc6724d43c807c220bc09f933e64c5fa0
Alee14 <Alee14498@gmail.com>
2018-01-13 20:44:36 -0500

    Added readline and you could speak to VT!

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./config.json');
+const readline = require('readline');
 const vtVersion = "1.0.0";
 const prefix = "vt!";
 
@@ -23,20 +24,61 @@ function setGame() {
 }
 
 client.on('ready', () => {
+  console.log(`Welcome to VTBot Terminal!`)
   console.log(`[STATUS] Bot is now ready!`);
   console.log(`[STATUS] Logged in as ${client.user.tag}!`);
+  console.log(`[STATUS] Prefix: ` + prefix)
+  console.log(`[STATUS] Token: ` + config.token)
   setGame();
   client.setInterval(setGame, 200000);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'VTBot> '
+  });
+  
+  rl.prompt();
+  
+  rl.on('line', (line) => {
+    switch (line.trim()) {
+      case 'help':
+        console.log('VTBot Terminal Help:\nhelp\nping');
+        break;
+      default:
+        console.log(`[ERROR] Unknown command: ${line.trim()}`);
+        break;
+    }
+    rl.prompt();
+  }).on('close', () => {
+    console.log('Have a great day!');
+    process.exit(0);
+  });
+  
 });
 
 client.on('message', msg => {
 
+    if (msg.mentions != null && msg.mentions.users != null) {
+        if (msg.mentions.users.has("401491921865801728")) {
+            if (msg.author.id == 361202413165608962) {
+                msg.reply("BEGONE. You're a undertale fan. :sob:");
+            } else {
+                if (msg.content.toLowerCase().includes("hello")) {
+                    msg.reply("Hi there.");
+            } else if (msg.content.toLowerCase().includes("shut") && msg.content.toLowerCase().includes("up")) {
+                msg.reply("Excuse me?")
+            }
+            }
+        }
+    }
+      
     if (msg.content == prefix + 'help') {
         const embed = new Discord.RichEmbed();
         embed.setTitle('VTBot Help')
         embed.setDescription('Every command you put in this bot must start with `' + prefix + '`')
         embed.addField('- General Commands', 'ping', true)
-         .setFooter("VTBot Copyright 2018. The version that VTBot's running is " + vtVersion + "!")
+        embed.setFooter("VTBot Copyright 2018. The version that VTBot's running is " + vtVersion + "!")
         embed.setColor("00C8FF")
         msg.channel.send({embed});
     }    
